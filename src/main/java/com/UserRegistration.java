078119e6444b17c92b3a3e4c4c39a600f7378887
sandeep786 <sadeeprajsingh05@gmail.com>
2020-10-22 14:02:38 +0530

    added to test case for Mobile number validation

--- src/main/java/com/UserRegistration.java
+++ src/main/java/com/UserRegistration.java
@@ -5,6 +5,8 @@ import java.util.regex.Pattern;
 public class UserRegistration {
 
     private static final String NAME_PATTERN = "^[A-Z][a-z]{2,}";
+    private static final String MOB_NUM_PATTERN = "^[0-9]{2}[ ]*[6-9][0-9]{9}$";
+
 
     public boolean validFirstName(String fName) {
         
@@ -15,6 +17,11 @@ public class UserRegistration {
     public boolean validLastName(String lName) {
         Pattern pattern=Pattern.compile(NAME_PATTERN);
         return pattern.matcher(lName).matches();
+    }
+
+    public boolean validMobileNumber(String mobNum) {
+        Pattern pattern=Pattern.compile(MOB_NUM_PATTERN);
+        return pattern.matcher(mobNum).matches();
     }
 }
 
--- src/test/java/com/UserRegistrationTest.java
+++ src/test/java/com/UserRegistrationTest.java
@@ -39,4 +39,25 @@ public class UserRegistrationTest {
         boolean result=userRegistration.validLastName("Ra");
         Assert.assertFalse(result);
     }
+
+    @Test
+    public void givenMobileNum_WhenProper_ShouldReturnTrue() {
+        UserRegistration userRegistration=new UserRegistration();
+        boolean result=userRegistration.validMobileNumber("91 8149600728");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void givenMobileNum_WhenSpaceNotMentioned_ShouldReturnTrue() {
+        UserRegistration userRegistration=new UserRegistration();
+        boolean result=userRegistration.validMobileNumber("918149600728");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void givenMobileNum_WhenLessDigits_ShouldReturnFalse() {
+        UserRegistration userRegistration=new UserRegistration();
+        boolean result=userRegistration.validMobileNumber("91 8149600");
+        Assert.assertFalse(result);
+    }
 }
