9defaa42aa61041f7e15be3216e2d9bc1db224a9
sandeep786 <sadeeprajsingh05@gmail.com>
2020-10-22 14:57:15 +0530

    added to check test case to validation of Password

--- src/main/java/com/UserRegistration.java
+++ src/main/java/com/UserRegistration.java
@@ -6,6 +6,8 @@ public class UserRegistration {
 
     private static final String NAME_PATTERN = "^[A-Z][a-z]{2,}";
     private static final String MOB_NUM_PATTERN = "^[0-9]{2}[ ]*[6-9][0-9]{9}$";
+    private static final String PASSWORD_PATTERN1 = "(?=.*[A-Z]+)(?=.*[0-9]+).{8,}";
+    private static final String PASSWORD_PATTERN2 = "[0-9a-zA-Z]*[^0-9a-zA-Z][0-9a-zA-Z]*";
 
 
     public boolean validFirstName(String fName) {
@@ -22,6 +24,12 @@ public class UserRegistration {
     public boolean validMobileNumber(String mobNum) {
         Pattern pattern=Pattern.compile(MOB_NUM_PATTERN);
         return pattern.matcher(mobNum).matches();
+    }
+
+    public boolean validPassword(String password) {
+        Pattern pattern1=Pattern.compile(PASSWORD_PATTERN1);
+        Pattern pattern2=Pattern.compile(PASSWORD_PATTERN2);
+        return pattern1.matcher(password).matches() && pattern2.matcher(password).matches();
     }
 }
 
--- src/test/java/com/UserRegistrationTest.java
+++ src/test/java/com/UserRegistrationTest.java
@@ -60,4 +60,62 @@ public class UserRegistrationTest {
         boolean result=userRegistration.validMobileNumber("91 8149600");
         Assert.assertFalse(result);
     }
+
+    @Test
+    public void givenPassword_WhenMin8Chars_ShouldReturnTrue() {
+        UserRegistration userRegistration=new UserRegistration();
+        boolean result=userRegistration.validPassword("oTp1f2*gf");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void givenPassword_WhenAtLeastOneUpperCase_ShouldReturnTrue() {
+        UserRegistration userRegistration=new UserRegistration();
+        boolean result=userRegistration.validPassword("Sandee@123");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void givenPassword_WhenAtleast1Number_ShouldReturnTru() {
+        UserRegistration userRegistration=new UserRegistration();
+        boolean result=userRegistration.validPassword("a1jsgdjT*");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void givenPassword_WhenHasExact1SpecialChar_ShouldReturnTrue() {
+        UserRegistration userRegistration=new UserRegistration();
+        boolean result=userRegistration.validPassword("#Sandeep123");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void  givenPassword_WhenHasLessChars_ShouldReturnFalse() {
+        UserRegistration userRegistration=new UserRegistration();
+        boolean result=userRegistration.validPassword("Raj12%");
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void givenPassword_WhenNoSpecialChar_ShouldReturnFalse() {
+        UserRegistration userRegistration=new UserRegistration();
+        boolean result=userRegistration.validPassword("Sandeep123");
+        Assert.assertFalse(result);
+
+    }
+
+    @Test
+    public void givenPassword_WhenNoNumeric_ShouldReturnFalse() {
+        UserRegistration userRegistration=new UserRegistration();
+        boolean result=userRegistration.validPassword("Sandeep@raj");
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void givenPassword_WhenHasMoreThan1SpecialChar_ShouldReturnFalse() {
+        UserRegistration userRegistration=new UserRegistration();
+        boolean result=userRegistration.validPassword("%*SandeepRaj");
+        Assert.assertFalse(result);
+
+    }
 }
