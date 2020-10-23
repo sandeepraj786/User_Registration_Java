00847b9572f3770edbf070cdc26ce7702bbdbe59
sandeep786 <sadeeprajsingh05@gmail.com>
2020-10-23 11:30:11 +0530

    added to jUnit test case for email validation

--- src/main/java/com/UserRegistration.java
+++ src/main/java/com/UserRegistration.java
@@ -8,6 +8,7 @@ public class UserRegistration {
     private static final String MOB_NUM_PATTERN = "^[0-9]{2}[ ]*[6-9][0-9]{9}$";
     private static final String PASSWORD_PATTERN1 = "(?=.*[A-Z]+)(?=.*[0-9]+).{8,}";
     private static final String PASSWORD_PATTERN2 = "[0-9a-zA-Z]*[^0-9a-zA-Z][0-9a-zA-Z]*";
+    private static final String EMAIL_PATTERN = "^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-z]{2,4})?$";
 
 
     public boolean validFirstName(String fName) {
@@ -30,6 +31,13 @@ public class UserRegistration {
         Pattern pattern1=Pattern.compile(PASSWORD_PATTERN1);
         Pattern pattern2=Pattern.compile(PASSWORD_PATTERN2);
         return pattern1.matcher(password).matches() && pattern2.matcher(password).matches();
+    }
+
+    public boolean validEmailId(String eMail) {
+        Pattern pattern=Pattern.compile(EMAIL_PATTERN);
+        return pattern.matcher(eMail).matches();
+
+
     }
 }
 
--- src/test/java/com/UserRegistrationTest.java
+++ src/test/java/com/UserRegistrationTest.java
@@ -2,8 +2,24 @@ package com;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+@RunWith(Parameterized.class)
 
 public class UserRegistrationTest {
+    private String eMail;
+    private boolean expectedResult;
+    private UserRegistration userRegistration;
+
+    public UserRegistrationTest(String eMail,boolean expectedResult) {
+        this.eMail = eMail;
+        this.expectedResult = expectedResult;
+    }
+
+
     @Test
     public void givenFirstName_WhenFirstLatterUpperCase_ShouldReturnTrue() {
         UserRegistration userRegistration =new  UserRegistration();
@@ -117,5 +133,37 @@ public class UserRegistrationTest {
         boolean result=userRegistration.validPassword("%*SandeepRaj");
         Assert.assertFalse(result);
 
+    }
+    @Parameterized.Parameters
+    public static Collection eMailIds_WithExpectedResult() {
+        return Arrays.asList(new Object[][] {   {"abc@yahoo.com",true},
+                {"abc-100@yahoo.com",true},
+                {"abc.100@yahoo.com",true},
+                {"abc111@abc.com",true},
+                {"abc-100@abc.net",true},
+                {"abc.100@abc.com.au",true},
+                {"abc@1.com",true},
+                {"abc@gmail.com.com",true},
+                {"abc+100@gmail.com",true},
+                {"abc",false},
+                {"abc@.com.my",false},
+                {"abc123@gmail.a",false},
+                {"abc123@.com",false},
+                {"abc123@.com.com",false},
+                {".abc@abc.com",false},
+                {"abc()*@gmail.com",false},
+                {"abc@%*.com",false},
+                {"abc..2002@gmail.com",false},
+                {"abc.@gmail.com",false},
+                {"abc@abc@gmail.com",false},
+                {"abc@gmail.com.1a",false},
+                {"abc@gmail.com.aa.au",false} });
+    }
+
+    @Test
+    public void givenEmailIds_WhenProper_ShouldReturnExpectedResult() {
+        UserRegistration userRegistration=new UserRegistration();
+        System.out.println("Expected Result:"+ expectedResult);
+        Assert.assertEquals(expectedResult, userRegistration.validEmailId(eMail));
     }
 }
